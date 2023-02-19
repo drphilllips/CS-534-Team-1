788ba26de1f66a5b8ae0c98b527628b72d9043ea
patrickm10 <pmejia@wpi.edu>
2023-02-18 20:39:53 -0500

    Path is correct but cost function still wrong

--- Project 1/SimpleProblemSolvingAgent.py
+++ Project 1/SimpleProblemSolvingAgent.py
@@ -1,97 +1,109 @@
-import heapq
-from math import sqrt
+import os
 
-from RomaniaMap import get_romania_map
+import RomaniaMap
+from SimpleProblemSolvingAgent import SimpleProblemSolvingAgent
 
 
-class SimpleProblemSolvingAgent:
+def prompt_for_map_file_directory():
     """
-    [Figure 3.1]
-    Abstract framework for a problem-solving agent.
+    Prompts user for the local directory of the
+    Romania map file
+    :return: directory of the map file
     """
+    map_file_directory = ""
+    # prompt user until receiving a valid map file directory
+    invalid_directory = True
+    while invalid_directory:
+        map_file_directory = input("Please enter where your map file is located in your local directory: ")
+        # if the path exists, we have a valid directory
+        if os.path.exists(map_file_directory):
+            invalid_directory = False
+        else:
+            print("Invalid directory.")
+    # return map file directory
+    return map_file_directory
 
-    def __init__(self, initial_state=None):
-        """State is an abstract representation of the state
-        of the world, and seq is the list of actions required
-        to get to a particular state from the initial state(root)."""
-        self.state = initial_state
-        self.seq = []
 
-    def __call__(self, percept):
-        """[Figure 3.1] Formulate a goal and problem, then
-        search for a sequence of actions to solve it."""
-        self.state = self.update_state(self.state, percept)
-        if not self.seq:
-            goal = self.formulate_goal(self.state)
-            problem = self.formulate_problem(self.state, goal)
-            self.seq = self.search(problem)
-            if not self.seq:
-                return None
-        return self.seq.pop(0)
+def in_romania_map(city):
+    """
+    Checks if city is in the romania_map file
+    :param city: string, city name
+    :return: boolean, whether city is in the map
+    """
+    # import romania map
+    romania_map = RomaniaMap.get_romania_map()
+    # check if the city is within the map nodes
+    city_in_romania_map = city in romania_map.nodes()
+    # return result
+    return city_in_romania_map
 
-    def update_state(self, state, percept):
-        raise NotImplementedError
 
-    def formulate_goal(self, state):
-        raise NotImplementedError
+def prompt_for_two_cities():
+    """
+    Prompts user for the two cities they would
+    like to navigate between
+    :return: two cities, as a tuple
+    """
+    # prompt user until receiving a valid pair of cities
+    invalid_cities = True
+    while invalid_cities:
+        city1 = input("Please enter a city from the romania_map: ")
+        city2 = input("Please enter another city from the romania_map: ")
+        # if two cities are different and in romania_map,
+        # we have a valid pair of cities
+        if city1 != city2 and \
+            in_romania_map(city1) and \
+                in_romania_map(city2):
+            invalid_cities = False
+        else:
+            print("Invalid cities.")
+    # combine cities into a tuple
+    two_cities = (city1, city2)
+    return two_cities
 
-    def formulate_problem(self, state, goal):
-        raise NotImplementedError
 
-    def search(self, problem):
-        raise NotImplementedError
+def main():
+    """
+    RomaniaCityApp provides interface for user to
+    interact with Greedy and Astar search algorithms
+    and find the best path between two cities in the
+    Romania Map provided
+    """
+    # prompt user for continuous run
+    best_path_again = True
+    while best_path_again:
+        # prompt user for two cities
+        two_cities = prompt_for_two_cities()
+        # create simple problem solving agent
+        spsa = SimpleProblemSolvingAgent()
+        # collect search results for greedy and astar search
+        print("Greedy Best-First Search")
+        greedy_search_results = spsa.best_graph_first_search(two_cities[0], two_cities[1])
+        astar_search_results = spsa.astar_search(two_cities, h=1)
+        # print astar search results
+        print("Astar Search")
+        astar_total_cost = astar_search_results[0]
+        astar_intermediate_cities = astar_search_results[1]
+        print("* Total Cost: " + str(astar_total_cost))
+        print("* Intermediate Cities: ")
+        for astar_intermediate_city in astar_intermediate_cities:
+            print("* - " + astar_intermediate_city)
+        # ask user if they would like to find the best path
+        # between any two cities again
+        invalid_yes_no_response = True
+        while invalid_yes_no_response:
+            best_path_again = input("Would you like to find the best path between any two cities again? (y/n): ")
+            if best_path_again == 'y' or best_path_again == 'Y':
+                best_path_again = True
+                invalid_yes_no_response = False
+            elif best_path_again == 'n' or best_path_again == 'N':
+                best_path_again = False
+                invalid_yes_no_response = False
+            else:
+                print("Invalid response, please respond with \'y\' or \'n\'.")
+    # terminate program, print thank you
+    print("Thank You for Using Our App")
 
-    # -- Implementation Below -- #
 
-    # Find distance between two cities using the Euclidean distance formula
-    # Parameters: self, first city, second city
-    # Returns the distance in the path between the two cities
-    def euclidean(self, city1, city2):
-        lat, long = city1
-        lat2, long2 = city2
-        return sqrt((lat2 - lat) ** 2 + (long2 - long) ** 2)
-
-    # Creates the search algorithm for the best greedy first search
-    # Parameters: self, start city, destination city
-    # Prints the path between the two cities and the total cost of the path
-    def best_graph_first_search(self, start, destination):
-        traveled_cities = []
-        city_check = set()
-        path = [(0, start)]
-        total_cost_so_far = 0
-
-        # Main loop
-        while path:
-            # Get city with lowest total cost
-            (total_cost, intermediate_city) = heapq.heappop(path)
-            if intermediate_city == destination:
-                traveled_cities.append(intermediate_city)
-                total_cost_so_far += total_cost
-                # Prints the search results and total cost of travel
-                print(f"* Path from {start} - {destination} ")
-                print(f"* {traveled_cities}")
-                print(f"* Total Cost: {total_cost_so_far} \n")
-                return traveled_cities
-
-            # Add city to visited cities
-            city_check.add(intermediate_city)
-            traveled_cities.append(intermediate_city)
-
-            for adj_city, distance in get_romania_map()[intermediate_city].items():
-                if adj_city not in traveled_cities:
-                    total_cost = distance + self.euclidean(get_romania_map().locations[adj_city],
-                                                           get_romania_map().locations[destination])
-                    # Add adjacent city to path
-                    heapq.heappush(path, (total_cost, adj_city))
-
-            # Update total cost so far
-            total_cost_so_far += total_cost
-
-        # If no path is found, return None
-        return None
-
-    def astar_search(self, problem, h):
-        total_cost = 1
-        intermediate_cities = ('int_city_1', 'int_city_2', '...', 'int_city_n')
-        search_results = (total_cost, intermediate_cities)
-        return search_results
+if __name__ == '__main__':
+    main()
