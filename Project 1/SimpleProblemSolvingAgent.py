4482e3c6d4512dfb818e8adfdd0cae0f5b00cd3b
patrickm10 <pmejia@wpi.edu>
2023-02-18 20:05:45 -0500

    Total cost function calculation needs work

--- Project 1/SimpleProblemSolvingAgent.py
+++ Project 1/SimpleProblemSolvingAgent.py
@@ -1,4 +1,6 @@
 import heapq
+from math import sqrt
+
 from RomaniaMap import get_romania_map
 
 
@@ -41,36 +43,50 @@ class SimpleProblemSolvingAgent:
 
     # -- Implementation Below -- #
 
+    # Find distance between two cities using the Euclidean distance formula
+    # Parameters: self, first city, second city
+    # Returns the distance in the path between the two cities
     def euclidean(self, node1, node2):
         lat, long = node1
         lat2, long2 = node2
-        return ((lat - lat2) ** 2 + (long2 - long2) ** 2) ** 0.5
+        return sqrt((lat2 - lat) ** 2 + (long2 - long) ** 2)
 
+    # Creates the search algorithm for the best greedy first search
+    # Parameters: self, start city, destination city
+    # Prints the path between the two cities and the total cost of the path
     def best_graph_first_search(self, start, destination):
         traveled_cities = []
         city_check = set()
         path = [(0, start)]
-        
+        total_cost_so_far = 0
+
         # Main loop
         while path:
             # Get city with lowest total cost
             (total_cost, intermediate_city) = heapq.heappop(path)
             if intermediate_city == destination:
                 traveled_cities.append(intermediate_city)
+                total_cost_so_far += total_cost
+                # Prints the search results and total cost of travel
+                print(f"* Path from {start} - {destination} ")
+                print(f"* {traveled_cities}")
+                print(f"* Total Cost: {total_cost_so_far} \n")
                 return traveled_cities
-            
+
             # Add city to visited cities
             city_check.add(intermediate_city)
             traveled_cities.append(intermediate_city)
-            print(get_romania_map())
+
             for adj_city, distance in get_romania_map()[intermediate_city].items():
                 if adj_city not in traveled_cities:
-                    total_cost = self.euclidean(adj_city,destination)
-                    
+                    total_cost = distance + self.euclidean(get_romania_map().locations[adj_city],
+                                                           get_romania_map().locations[destination])
                     # Add adjacent city to path
                     heapq.heappush(path, (total_cost, adj_city))
-                    print(path, total_cost)
-                    
+
+            # Update total cost so far
+            total_cost_so_far += total_cost
+
         # If no path is found, return None
         return None
 
