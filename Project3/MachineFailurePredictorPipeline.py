87c875ca8d269efbd9172bf0fd998d6d8aeb5a46
Alex <aralvarez023@gmail.com>
2023-04-08 13:44:58 -0400

    Update MachineFailurePredictorPipeline.py

--- Project3/MachineFailurePredictorPipeline.py
+++ Project3/MachineFailurePredictorPipeline.py
@@ -29,22 +29,30 @@ def main():
     # ---
     # train-test split
     X_train, X_test, y_train, y_test = train_test_split(X_over, np.ravel(y_over), test_size=0.3, random_state=42)
+
+    # MLP Classifier
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-
-    svclassifier = SVC(kernel='linear')
-    svclassifier.fit(X_train, y_train)
-    y_pred = svclassifier.predict(X_test)
-
+    print("MLP Classifier")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
     f1 = f1_score(y_test, y_pred)
     print("F1 Score:", f1)
 
-    X, y = make_classification(n_samples=100, n_features=4, n_informative=2, n_redundant=0,
+    # SV Classifier
+    svclassifier = SVC(kernel='linear')
+    svclassifier.fit(X_train, y_train)
+    y_pred = svclassifier.predict(X_test)
+    print("SV Classifier")
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+    f1 = f1_score(y_test, y_pred)
+    print("F1 Score:", f1)
+
+    X_class, y_class = make_classification(n_samples=100, n_features=4, n_informative=2, n_redundant=0,
                                random_state=0, shuffle=False)
-    clf = BaggingClassifier(estimator=SVC(), n_estimators=10, random_state=0).fit(X, y)
+    clf = BaggingClassifier(estimator=SVC(), n_estimators=10, random_state=0).fit(X_class, y_class)
     clf.predict([[0, 0, 0, 0]])
 
     ml_table = pd.DataFrame(columns=['ML Trained Model', 'Its Best Set of Parameters', 'Its F1-score on the 5-fold '
