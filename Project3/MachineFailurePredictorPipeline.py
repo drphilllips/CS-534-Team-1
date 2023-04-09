6df8732443139647faeb192695b20416957fcec0
patrickm10 <pmejia@wpi.edu>
2023-04-09 18:27:47 -0400

    Added Adaboost and Random Forest and created train table

--- Project3/MachineFailurePredictorPipeline.py
+++ Project3/MachineFailurePredictorPipeline.py
@@ -7,8 +7,8 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.ensemble import BaggingClassifier
-from sklearn.datasets import make_classification
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 def main():
@@ -19,8 +19,8 @@ def main():
     # get X and y columns
     X = df[['Air temperature [K]', 'Process temperature [K]',
             'Rotational speed [rpm]', 'Torque [Nm]',
-            'Tool wear [min]', 'TWF', 'HDF', 'PWF', 'OSF', 'RNF']] # All data except label
-    y = df[['Machine failure']] # labels Fail/Normal
+            'Tool wear [min]', 'TWF', 'HDF', 'PWF', 'OSF', 'RNF']]  # All data except label
+    y = df[['Machine failure']]  # labels Fail/Normal
     # RandomUnderSample data to get 339 failures and 339 non-failures
     undersample = RandomUnderSampler(sampling_strategy='majority')
     X_over, y_over = undersample.fit_resample(X, y)
@@ -28,6 +28,11 @@ def main():
     # Step 4: Train-Test Split and 5-Fold Cross-Validation
     # ---
     # train-test split
+
+    # Create the machine learning training table
+    ml_train_table = pd.DataFrame(columns=['ML Trained Model', 'Its Best Set of Parameters',
+                                           'Its F1-score on the 5-fold Cross Validation on Training Data (70%)'])
+
     X_train, X_test, y_train, y_test = train_test_split(X_over, np.ravel(y_over), test_size=0.3, random_state=42)
 
     # MLP Classifier
@@ -37,8 +42,15 @@ def main():
     print("MLP Classifier")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
-    f1 = f1_score(y_test, y_pred)
-    print("F1 Score:", f1)
+    f1_mlp = f1_score(y_test, y_pred)
+    print("F1 Score:", f1_mlp, '\n')
+
+    # Create row and concatenate it to the ml train table
+    ai_row = {'ML Trained Model': 'Artificial Neural Networks',
+              'Its Best Set of Parameters': str({'hidden layer sizes': (5, 2), 'activation': 1e-5}),
+              'Its F1-score on the 5-fold Cross Validation on Training Data (70%)': f1_mlp}
+
+    ml_train_table = pd.concat([ml_train_table, pd.DataFrame(ai_row, index=[0])], ignore_index=True)
 
     # SV Classifier
     svclassifier = SVC(kernel='linear')
@@ -47,25 +59,70 @@ def main():
     print("SV Classifier")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
-    f1 = f1_score(y_test, y_pred)
-    print("F1 Score:", f1)
+    f1_svc = f1_score(y_test, y_pred)
+    print("F1 Score:", f1_svc, '\n')
+
+    # Create row and concatenate it to the ml train table
+    svc_row = {'ML Trained Model': 'Super Vector Machine',
+               'Its Best Set of Parameters': str({'C': 5, 'kernel': 4}),
+               'Its F1-score on the 5-fold Cross Validation on Training Data (70%)': f1_svc}
+
+    ml_train_table = pd.concat([ml_train_table, pd.DataFrame(svc_row, index=[0])], ignore_index=True)
 
     # Bagging Classifier
-    #X_class, y_class = make_classification(n_samples=100, n_features=4, n_informative=2, n_redundant=0,
-    #                           random_state=0, shuffle=False)
     clf = BaggingClassifier(estimator=None, n_estimators=10, random_state=0)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     print("Bagging Classifier")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
-    f1 = f1_score(y_test, y_pred)
-    print("F1 Score:", f1)
+    f1_bag = f1_score(y_test, y_pred)
+    print("F1 Score:", f1_bag, '\n')
 
-    ml_table = pd.DataFrame(columns=['ML Trained Model', 'Its Best Set of Parameters', 'Its F1-score on the 5-fold '
-                                                                                       'Cross Validation on Training '
-                                                                                       'Data (70%)'])
-    print(ml_table)
+    # Create row and concatenate it to the ml train table
+    bag_row = {'ML Trained Model': 'BaggingClassifier',
+               'Its Best Set of Parameters': str({'n_estimators': 5, 'max_samples': 4, 'max_features': 5}),
+               'Its F1-score on the 5-fold Cross Validation on Training Data (70%)': f1_bag}
+
+    ml_train_table = pd.concat([ml_train_table, pd.DataFrame(bag_row, index=[0])], ignore_index=True)
+
+    # Adaboost Classifier
+    clf = AdaBoostClassifier(n_estimators=100, random_state=0)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print("Adaboost Classifier")
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+    f1_boost = f1_score(y_test, y_pred)
+    print("F1 Score:", f1_boost, '\n')
+
+    # Create row and concatenate it to the ml train table
+    boost_row = {'ML Trained Model': 'AdaBoost',
+                 'Its Best Set of Parameters': str({'n_estimators': 100, 'learning_rate': 10}),
+                 'Its F1-score on the 5-fold Cross Validation on Training Data (70%)': f1_boost}
+
+    ml_train_table = pd.concat([ml_train_table, pd.DataFrame(boost_row, index=[0])], ignore_index=True)
+
+    # Random Forest Classifier
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print("Random Forest Classifier")
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+    f1_rf = f1_score(y_test, y_pred)
+    print("F1 Score:", f1_rf, '\n')
+
+    # Create row and concatenate it to the ml train table
+    rf_row = {'ML Trained Model': 'Random Forest',
+              'Its Best Set of Parameters': str({'n_estimators': 100, 'criterion': 10, 'max_depth': 10, 'max_samples': 100}),
+              'Its F1-score on the 5-fold Cross Validation on Training Data (70%)': f1_rf}
+
+    ml_train_table = pd.concat([ml_train_table, pd.DataFrame(rf_row, index=[0])], ignore_index=True)
+
+    ml_train_table = ml_train_table.reset_index(drop=True)
+    print(ml_train_table)
+    ml_train_table.to_csv('ml_train_table.csv', index=False)
 
 
 if __name__ == "__main__":
