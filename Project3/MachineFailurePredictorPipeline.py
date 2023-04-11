ee373ee673aa39513dddf27172686e414a791f07
Alex <aralvarez023@gmail.com>
2023-04-10 23:08:33 -0400

    cleaned up svc, ada, rf

--- Project3/MachineFailurePredictorPipeline.py
+++ Project3/MachineFailurePredictorPipeline.py
@@ -50,7 +50,6 @@ def main():
     grid.fit(X_train, y_train)
     # Generate scores from best parameters
     y_pred = grid.predict(X_test)
-    print(grid.best_params_)
     print("MLP Classifier")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
@@ -67,21 +66,23 @@ def main():
     ml_train_table = pd.concat([ml_train_table, pd.DataFrame(ai_row, index=[0])], ignore_index=True)
 
     # SV Classifier
-    parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 5, 10, 20]}
+    parameters = {'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+                  'C': [0.001, 0.01, 0.1, 1, 5, 10]}
     svc = SVC()
     grid = GridSearchCV(svc, parameters, cv=5)
     grid.fit(X_train, y_train)
     y_pred = grid.predict(X_test)
-    print(grid.get_params())
     print("SV Classifier")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
     f1_svc = f1_score(y_test, y_pred)
     print("F1 Score:", f1_svc, '\n')
+    params_svc = grid.best_params_
 
     # Create row and concatenate it to the ml train table
     svc_row = {'ML Trained Model': 'Super Vector Machine',
-               'Its Best Set of Parameters': str({'C': 5, 'kernel': 4}),
+               'Its Best Set of Parameters': str({'C': params_svc['C'],
+                                                  'kernel': params_svc['kernel']}),
                'Its F1-score on the 5-fold Cross Validation on Training Data (70%)': f1_svc}
 
     ml_train_table = pd.concat([ml_train_table, pd.DataFrame(svc_row, index=[0])], ignore_index=True)
@@ -104,35 +105,55 @@ def main():
     ml_train_table = pd.concat([ml_train_table, pd.DataFrame(bag_row, index=[0])], ignore_index=True)
 
     # Adaboost Classifier
-    clf = AdaBoostClassifier(n_estimators=100, random_state=0)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
+    parameters = {
+        'n_estimators': [1, 5, 10, 25, 50, 100],
+        'learning_rate': [0.01, 0.1, 1.0, 5.0, 10.0]
+    }
+    ada = AdaBoostClassifier()
+    grid = GridSearchCV(ada, parameters, cv=5)
+    grid.fit(X_train, y_train)
+    y_pred = grid.predict(X_test)
     print("Adaboost Classifier")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
     f1_boost = f1_score(y_test, y_pred)
     print("F1 Score:", f1_boost, '\n')
+    params_ada = grid.best_params_
 
     # Create row and concatenate it to the ml train table
     boost_row = {'ML Trained Model': 'AdaBoost',
-                 'Its Best Set of Parameters': str({'n_estimators': 100, 'learning_rate': 10}),
+                 'Its Best Set of Parameters': str({'n_estimators': params_ada['n_estimators'],
+                                                    'learning_rate': params_ada['learning_rate']}),
                  'Its F1-score on the 5-fold Cross Validation on Training Data (70%)': f1_boost}
 
     ml_train_table = pd.concat([ml_train_table, pd.DataFrame(boost_row, index=[0])], ignore_index=True)
 
     # Random Forest Classifier
-    clf = RandomForestClassifier(max_depth=2, random_state=0)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
+    parameters = {
+        'n_estimators': [10, 25, 50, 100],
+        'criterion': ['gini', 'entropy', 'log_loss'],
+        'max_features': [1, 2, 3],
+        'max_depth': [1, 2, 5],
+        'max_samples': [1, 5, 10, 20]
+    }
+    rfc = RandomForestClassifier()
+    grid = GridSearchCV(rfc, parameters, cv=5)
+    grid.fit(X_train, y_train)
+    y_pred = grid.predict(X_test)
     print("Random Forest Classifier")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
     f1_rf = f1_score(y_test, y_pred)
     print("F1 Score:", f1_rf, '\n')
+    params_rfc = grid.best_params_
 
     # Create row and concatenate it to the ml train table
     rf_row = {'ML Trained Model': 'Random Forest',
-              'Its Best Set of Parameters': str({'n_estimators': 100, 'criterion': 10, 'max_depth': 10, 'max_samples': 100}),
+              'Its Best Set of Parameters': str({'n_estimators': params_rfc['n_estimators'],
+                                                 'criterion': params_rfc['criterion'],
+                                                 'max_features': params_rfc['max_features'],
+                                                 'max_depth': params_rfc['max_depth'],
+                                                 'max_samples': params_rfc['max_samples']}),
               'Its F1-score on the 5-fold Cross Validation on Training Data (70%)': f1_rf}
 
     ml_train_table = pd.concat([ml_train_table, pd.DataFrame(rf_row, index=[0])], ignore_index=True)
