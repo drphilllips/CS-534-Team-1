add455e989a7fbf235a3b4c079fc1008e4d8e1e7
patrickm10 <pmejia@wpi.edu>
2023-04-10 23:33:04 -0400

    ML model and test table should be done

--- Project3/MachineFailurePredictorPipeline.py
+++ Project3/MachineFailurePredictorPipeline.py
@@ -11,7 +11,6 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 
-
 def main():
     # ------
     # Step 3: Load Data and Randomly Under-Sample the Majority
@@ -167,23 +166,15 @@ def main():
     ml_train_table = ml_train_table.reset_index(drop=True)
     print(ml_train_table)
     ml_train_table.to_csv('ml_train_table.csv', index=False)
-    
+
     ml_test_table = pd.DataFrame(columns=['ML Trained Model', 'Its Best Set of Parameters',
-                                           'Its F1-score on the 5-fold Cross Validation on Training Data (70%)'])
+                                          'Its F1-score on the 5-fold Cross Validation on Training Data (70%)'])
 
     X_train, X_test, y_train, y_test = train_test_split(X_over, np.ravel(y_over), test_size=0.3, random_state=42)
 
     # MLP Classifier
-    parameters = {'solver': ['lbfgs', 'sgd', 'adam'],
-                  'alpha': [0.00001, 0.00005],
-                  'hidden_layer_sizes': [(10, 2), (20, 2)],
-                  'learning_rate': ['constant', 'adaptive'],
-                  'activation': ['tanh', 'relu'],
-                  'random_state': [1],
-                  'early_stopping': [True]}
-    mlp = MLPClassifier(max_iter=5000)
-    grid = GridSearchCV(mlp, parameters, cv=5)
-    grid.fit(X_test, y_test)
+    mlp = MLPClassifier(solver='adam', activation='tanh', max_iter=5000)
+    mlp.fit(X_test, y_test)
     y_pred = grid.predict(X_test)
     print("MLP Classifier")
     accuracy = accuracy_score(y_test, y_pred)
@@ -193,17 +184,17 @@ def main():
 
     # Create row and concatenate it to the ml train table
     ai_row = {'ML Trained Model': 'Artificial Neural Networks',
-              'Its Best Set of Parameters': str({'hidden layer sizes': (5, 2), 'activation': 1e-5}),
+              'Its Best Set of Parameters': str({'hidden layer sizes': (200, 2), 'activation': 'tanh'}),
               'Its F1-score on the 5-fold Cross Validation on Training Data (30%)': f1_mlp}
 
     ml_test_table = pd.concat([ml_test_table, pd.DataFrame(ai_row, index=[0])], ignore_index=True)
 
     # SV Classifier
-    parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 5, 10, 20]}
-    svc = SVC()
-    grid = GridSearchCV(svc, parameters, cv=5)
-    grid.fit(X_test, y_test)
-    y_pred = grid.predict(X_test)
+    clf = BaggingClassifier(estimator=None, n_estimators=10, random_state=0)
+    clf.fit(X_test, y_test)
+    svc = SVC(C=0.1, kernel='linear')
+    svc.fit(X_test, y_test)
+    y_pred = svc.predict(X_test)
     print("SV Classifier")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
@@ -212,13 +203,13 @@ def main():
 
     # Create row and concatenate it to the ml train table
     svc_row = {'ML Trained Model': 'Super Vector Machine',
-               'Its Best Set of Parameters': str({'C': 5, 'kernel': 4}),
+               'Its Best Set of Parameters': str({'C': 0.1, 'kernel': 'linear'}),
                'Its F1-score on the 5-fold Cross Validation on Training Data (30%)': f1_svc}
 
     ml_test_table = pd.concat([ml_test_table, pd.DataFrame(svc_row, index=[0])], ignore_index=True)
 
     # Bagging Classifier
-    clf = BaggingClassifier(estimator=None, n_estimators=10, random_state=0)
+    clf = BaggingClassifier(estimator=None, n_estimators=20, max_samples=20, max_features=5, random_state=0)
     clf.fit(X_test, y_test)
     y_pred = clf.predict(X_test)
     print("Bagging Classifier")
@@ -229,7 +220,7 @@ def main():
 
     # Create row and concatenate it to the ml train table
     bag_row = {'ML Trained Model': 'BaggingClassifier',
-               'Its Best Set of Parameters': str({'n_estimators': 5, 'max_samples': 4, 'max_features': 5}),
+               'Its Best Set of Parameters': str({'n_estimators': 20, 'max_samples': 200, 'max_features': 5}),
                'Its F1-score on the 5-fold Cross Validation on Training Data (30%)': f1_bag}
 
     ml_test_table = pd.concat([ml_test_table, pd.DataFrame(bag_row, index=[0])], ignore_index=True)
@@ -246,13 +237,13 @@ def main():
 
     # Create row and concatenate it to the ml train table
     boost_row = {'ML Trained Model': 'AdaBoost',
-                 'Its Best Set of Parameters': str({'n_estimators': 100, 'learning_rate': 10}),
+                 'Its Best Set of Parameters': str({'n_estimators': 25, 'learning_rate': 0.1}),
                  'Its F1-score on the 5-fold Cross Validation on Training Data (30%)': f1_boost}
 
     ml_test_table = pd.concat([ml_test_table, pd.DataFrame(boost_row, index=[0])], ignore_index=True)
 
     # Random Forest Classifier
-    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf = RandomForestClassifier(n_estimators=50, criterion='log_loss', max_features=1, max_depth=5, max_samples=20, random_state=0)
     clf.fit(X_test, y_test)
     y_pred = clf.predict(X_test)
     print("Random Forest Classifier")
@@ -264,7 +255,7 @@ def main():
     # Create row and concatenate it to the ml train table
     rf_row = {'ML Trained Model': 'Random Forest',
               'Its Best Set of Parameters': str(
-                  {'n_estimators': 100, 'criterion': 10, 'max_depth': 10, 'max_samples': 100}),
+                  {'n_estimators': 50, 'criterion': 'log_loss', 'max_depth': 5, 'max_samples': 20}),
               'Its F1-score on the 5-fold Cross Validation on Training Data (30%)': f1_rf}
 
     ml_test_table = pd.concat([ml_test_table, pd.DataFrame(rf_row, index=[0])], ignore_index=True)
