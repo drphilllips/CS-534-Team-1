96f65251f76f4c6497affaccdc24be10ce75b39b
drphilllips <drphillips@wpi.edu>
2023-04-23 21:56:15 -0400

    Final documentation :D

--- Project4/AlexNetPredictorPipeline.py
+++ Project4/AlexNetPredictorPipeline.py
@@ -7,6 +7,7 @@ import numpy as np
 
 
 def main():
+    # Create transformation for images to tensor
     transformation = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.RandomHorizontalFlip(),
@@ -15,12 +16,6 @@ def main():
     ])
 
     data = ImageFolder('complete_mednode_dataset', transform=transformation)
-    #print(data[0][1] == 0)
-
-    #train_size = int(0.60 * len(train_data))
-    #test_size = len(train_data) - train_size
-    #train_data, test_data = torch.utils.data.random_split(train_data, [train_size, test_size])
-    #print(len(train_data))
 
     # Select 50 melanoma and 50 naevus images for training
     # Need to build in indices in order to indicate which training photos are included
@@ -78,11 +73,9 @@ def main():
         train_labels.append(0)
     train_tensor = np.array(torch.stack(train_photos))
     train_labels = np.array(train_labels)
-    #print(train_tensor)
-    #print(len(train_tensor))
 
     # run alexnet on train tensor with a range of dropouts
-    # TODO need to do 5-Fold to find the best dropout number
+    # 5-Fold to find the best dropout number
 
     # Perform 5-fold cross-validation to find the best dropout rate
     kfold = KFold(n_splits=5, shuffle=True)
@@ -93,45 +86,30 @@ def main():
     best_accuracy = 0.0
 
     for d in dropout_rates:
-        #print('Dropout rate: ', d)
         fold_accuracy = []
         # For each fold split of train and validation data...
         for train_idx, val_idx in kfold.split(train_tensor, train_labels):
             train_idx = train_idx.astype(int)
-            val_idx = val_idx.astype(int)
             # Store train and validation tensors and labels
             train_x, train_y = train_tensor[train_idx], train_labels[train_idx]
-            val_x, val_y = train_tensor[val_idx], train_labels[val_idx]
 
             train_x = torch.tensor(train_x)
             train_y = torch.tensor(train_y)
-            val_x = torch.tensor(val_x)
-            val_y = torch.tensor(val_y)
 
-            # TODO: build the AlexNet model
+            # build the AlexNet model
             model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', num_classes=2, dropout=d)
             model.eval()
 
             with torch.no_grad():
                 output = model(train_x)
-            #print(output[0])
 
-            # model = AlexNet(weights=None, include_top=True, input_shape=(224, 224, 3), classes=2)
-            #
-            # model.compile(optimizer=tf.keras.optimizers.Adam(),
-            #               loss='binary_crossentropy',
-            #               metrics=['accuracy'])
-            #
-            # model.fit(train_x, train_y, epochs=10, batch_size=32, verbose=0)
-
-            # score = model.evaluate(val_x, val_y, verbose=0)
-            # fold_accuracy.append(score[1])
-
+            # train the model
             probabilities = torch.nn.functional.softmax(output, dim=1)
             predictions = torch.argmax(probabilities, dim=1)
             accuracy = torch.sum(predictions == train_y).item() / len(train_y)
             fold_accuracy.append(accuracy)
 
+            # update fold results
             average_accuracy = np.mean(fold_accuracy)
             if average_accuracy > best_accuracy:
                 best_dropout_rate = d
@@ -140,7 +118,7 @@ def main():
     print("Best dropout rate:", best_dropout_rate)
     print("Best accuracy:", best_accuracy, '\n')
 
-    # Compile train photos into 100x3x256x256 tensor, no labels
+    # Compile test photos into 100x3x256x256 tensor
     test_photos = []
     test_labels = []
     for m in melanoma_test_photos:
@@ -152,6 +130,7 @@ def main():
     test_tensor = torch.stack(test_photos)
     test_labels = torch.Tensor(np.array(test_labels))
 
+    # Train and test model using the best dropout rate
     test_model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', num_classes=2, dropout=best_dropout_rate)
     test_model.eval()
     with torch.no_grad():
